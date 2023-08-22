556f4462875ca86fc85e4e4514f733686df0cf67
rogov alexandr <aarogov@alfabank.ru>
2023-08-23 02:20:26 +0300

    feat(small) add info about restApi

--- src/main/java/gmail/rohaualexandr/distancecalculatorapi/controller/DistanceController.java
+++ src/main/java/gmail/rohaualexandr/distancecalculatorapi/controller/DistanceController.java
@@ -2,17 +2,24 @@ package gmail.rohaualexandr.distancecalculatorapi.controller;
 
 import gmail.rohaualexandr.distancecalculatorapi.controller.dto.ResponseDistance;
 import gmail.rohaualexandr.distancecalculatorapi.service.DistanceService;
+
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping(value = DistanceController.URL)
+@Tag(name = "Distance Calculator API")
 @RestController
 public class DistanceController {
 
     public static final String URL = "/distance";
 
     private final DistanceService distanceService;
+
+    @Operation(summary = "Get a distance two point by latitude and longitude ", description = "Returns a distance in km")
 
     @GetMapping()
     public ResponseDistance getDistance(
--- src/main/java/gmail/rohaualexandr/distancecalculatorapi/controller/dto/ResponseDistance.java
+++ src/main/java/gmail/rohaualexandr/distancecalculatorapi/controller/dto/ResponseDistance.java
@@ -1,10 +1,12 @@
 package gmail.rohaualexandr.distancecalculatorapi.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
 public class ResponseDistance {
+    @Schema(name = "Distance", example = "1", required = true)
     private double distance;
 }
